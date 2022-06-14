5885c52df0dbc5549fa0ab1302b48795713eafb5
DracKunn <brecht.vanrie@gmail.com>
2022-06-14 16:39:09 +0200

    turned address into a record

--- src/main/java/com/switchfully/eurder/users/Address.java
+++ src/main/java/com/switchfully/eurder/users/Address.java
@@ -1,15 +1,4 @@
 package com.switchfully.eurder.users;
 
-public class Address {
-    private final String streetName;
-    private final int streetNumber;
-    private final String postalCode;
-    private final String city;
-
-    public Address(String streetName, int streetNumber, String postalCode, String city) {
-        this.streetName = streetName;
-        this.streetNumber = streetNumber;
-        this.postalCode = postalCode;
-        this.city = city;
-    }
+public record Address(String streetName,int streetNumber,String postalCode,String city) {
 }
