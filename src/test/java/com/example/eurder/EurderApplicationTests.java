9ccc0a7cc7c4c800beffb8d6508717c34ea2a2df
DracKunn <brecht.vanrie@gmail.com>
2022-06-14 10:40:02 +0200

    created project

---
+++ src/test/java/com/example/eurder/EurderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.eurder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//not implemented yet
+	}
+
+}
