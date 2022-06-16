8a612ae401318942820e3d1f8e213f37d9af3081
DracKunn <brecht.vanrie@gmail.com>
2022-06-16 16:35:58 +0200

    searching for a nullpointer exception when registering a customer.

--- src/main/java/com/switchfully/eurder/users/customer/CustomerMapper.java
+++ src/main/java/com/switchfully/eurder/users/customer/CustomerMapper.java
@@ -2,15 +2,21 @@ package com.switchfully.eurder.users.customer;
 
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 
 @Component
 public class CustomerMapper {
+    private final java.util.logging.Logger logger = Logger.getLogger(this.getClass().getName());
+
 
     public CustomerDTO customerToCustomerDTO(Customer customer){
+        logger.info("Customer transformed to DTO");
         return new CustomerDTO(customer.getName(), customer.getEmail(), customer.getAddress(), customer.getPhoneNumber());
     }
 
     public Customer customerDTOToCustomer(CustomerDTO customerDTO){
+        logger.info("DTO transformed to Customer");
         return new Customer(customerDTO.getName(),customerDTO.getEmail(),customerDTO.getAddress(),customerDTO.getPhoneNumber());
     }
 }
